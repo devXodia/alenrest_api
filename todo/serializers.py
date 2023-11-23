b5753d02d51828950f3bca3c3e29366b6bb52cf3
Alen Alduk <youstylez@gmail.com>
2023-11-23 13:25:57 +0100

    POST functionality todo

--- todo/serializers.py
+++ todo/serializers.py
@@ -3,7 +3,26 @@ from rest_framework import serializers
 from django.contrib.auth.models import User, Group
 
 
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        # user = request.user
+        fields = [
+            "id",
+            "first_name",
+            "last_name",
+            "username",
+            "email",
+        ]
+
+
 class TodoSerializer(serializers.HyperlinkedModelSerializer):
+    # user = CurrentUserDefault()
+    user = serializers.PrimaryKeyRelatedField(
+        read_only=True, default=serializers.CurrentUserDefault()
+    )
+
     class Meta:
         model = Todo
-        fields = ["id", "title", "description", "created_at"]
+        # user = request.user
+        fields = ["id", "title", "description", "created_at", "user", "time_passed"]
--- todo/views.py
+++ todo/views.py
@@ -1,5 +1,7 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from rest_framework import viewsets
+from django.core import serializers
 from rest_framework import permissions
 from .models import Todo
 from .serializers import TodoSerializer
@@ -8,6 +10,24 @@ from .serializers import TodoSerializer
 
 
 class TodoViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+
     queryset = Todo.objects.all().order_by("-created_at")
     serializer_class = TodoSerializer
     permission_classes = []  # permissions.IsAuthenticated
+
+    def create(self, request):
+        todo = Todo.objects.create(
+            title=request.POST.get("title", ""),
+            description=request.POST.get("description", ""),
+            user=request.user,
+        )
+        serialized_obj = serializers.serialize(
+            "json",
+            [
+                todo,
+            ],
+        )
+        return HttpResponse(serialized_obj, content_type="application/json")
